a388113b2174e554b022ce0bfb072ede475ae4b3
Samuel Xu (samuel-x) <roflollies@gmail.com>
2018-05-08 00:42:48 +1000

    Added Firebase functionality and cleaned up a bunch of things

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 #Python libraries that we need to import for our bot
-import random
 import os
-import json
-import pyrebase
 from flask import Flask, request
 from pymessenger.bot import Bot
 from googletrans import Translator
-import config
-import languages
+import database
 
 app = Flask(__name__)
 
@@ -16,10 +12,6 @@ translator = Translator()
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 TESTING_MODE = os.environ['TESTING_MODE']
-
-MEMBERS = {}
-
-
 
 bot = Bot(ACCESS_TOKEN)
 
@@ -47,24 +39,12 @@ def receive_message():
                     # check if this is a
                     # We've done all the parsing input, so check if they're new
                     if message_text[0] == "/":
-                        if message_text == "/leave_room":
-                            leave_room(sender_id)
-                        elif message_text == "/join_room":
-                            print("Please enter your room number")
-                        elif message_text.split(" ")[0] == "/join_room":
-                            room_id = message_text.split(" ", 1)[1]
-                            join_room(sender_id, room_id)
-                        elif message_text.split(" ")[0] == "/set_name":
-                            new_name = message_text.split(" ", 1)[1]
-                            change_name(sender_id, new_name)
-                        elif message_text.split(" ")[0] == "/set_lang":
-                            new_lang = message_text.split(" ", 1)[1]
-                            set_language(sender_id, new_lang)
+                        database.parse_command(sender_id, message_text)
                     elif int(TESTING_MODE):
                         send_translated_message(sender_id, message_text, "*TEST*")
-                    elif not check_new_user(message['sender']['id']):
-                        print("Sending message to ", get_room(sender_id))
-                        send_room_message(message_text, sender_id, get_room(sender_id))
+                    elif not database.check_new_user(message['sender']['id']):
+                        print("Sending message to ", database.get_user_room(sender_id))
+                        send_room_message(message_text, sender_id)
 
                 else:
                     print("This isn't a text message")
@@ -72,106 +52,21 @@ def receive_message():
 
     return "Message Processed"
 
-def get_room(user_id):
-    with open('rooms.json', 'r') as room_data:
-        rooms = json.load(room_data)
-        room_data.close()
-        for key in rooms:
-            print(key)
-            if user_id in rooms[key]:
-                return key
-
-def get_name(sender_id):
-    with open('members.json', 'r') as member_data:
-        members = json.load(member_data)
-        member_data.close()
-        return "*" + str(members[sender_id]) + "*: "
-
-
-
-def change_name(user_id, new_name):
-    with open('members.json', 'r') as member_data:
-        members = json.load(member_data)
-        members[user_id] = new_name
-        with open('members.json', 'w') as member_data:
-            json.dump(members, member_data)
-        member_data.close()
-        print("User ", user_id, " changed their name to ", member_data)
-
-def leave_room(user_id):
-    with open('rooms.json', 'r') as room_data:
-        rooms = json.load(room_data)
-        for users, room in rooms.items():
-            for user in users:
-                if user == user_id:
-                    room.pop(user, None)
-                    room_data.close()
-                    with open('rooms.json', 'w') as room_data:
-                        json.dump(room, room_data)
-                    room_data.close()
-                    print("User ", user_id, " removed from room ", room)
-
-def join_room(user_id, room_id):
-    with open('rooms.json', 'r') as room_data:
-        rooms = json.load(room_data)
-        if rooms.get(room_id):
-            rooms[room_id].append(user_id)
-        else:
-            rooms[room_id] = [user_id]
-        room_data.close()
-        with open('rooms.json', 'w') as room_data:
-            json.dump(rooms, room_data)
-        room_data.close()
-        print("User ", user_id, " added to room ", room_id)
-
-def send_room_message(message_text, user_id, room_id):
-    with open('rooms.json', 'r') as rooms:
-        room = json.load(rooms).get(room_id)
-        rooms.close()
-        if room == None:
-            send_message(user_id, "Please join a room first!")
-        else:
-            for user in room:
-                if user != user_id:
-                    send_translated_message(user, message_text, get_name(user_id))
+def send_room_message(message_text, user_id):
+    room_id = database.get_user_room(user_id)
+    if room_id == None:
+        send_message(user_id, "Please join a room first! Do it with '/join_room <room_number>'")
+    else:
+        for user in database.get_room(room_id):
+            if user != user_id:
+                send_translated_message(user, message_text, database.get_name(user_id))
 
 def send_translated_message(user_id, message_text, name):
-    with open('language.json', 'r') as language_data:
-        language = json.load(language_data).get(user_id)
-        language_data.close()
-        send_message(user_id, name + translator.translate(message_text, dest=language).text)
-
-def check_new_user(user_id):
-    with open('members.json', 'r') as member_data:
-        members = json.load(member_data)
-        member_data.close()
-        if user_id not in members.keys():
-            members[user_id] = 1
-            with open('members.json', 'w') as member_data:
-                json.dump(members, member_data)
-            member_data.close()
-            return True
-        else:
-            return False
-
-
-def check_room(room_name, user_id):
-    with open('rooms.json', 'r+') as rooms:
-        d = json.load(rooms)
-        if user_id not in d.get(room_name):
-            return False
-        return True
-
-def set_language(user_id, language):
-    with open('language.json', 'r') as language_data:
-        d = json.load(language_data)
-        d[user_id] = language
-        language_data.close()
-        with open('language.json', 'w') as language_data:
-            json.dump(d, language_data)
-        language_data.close()
-        message = "Your language is now: " + languages.LANGUAGES[d[user_id]]
-        send_message(user_id, message)
+    lang = database.get_lang(user_id)
+    if lang == None:
+        send_message(user_id, name + translator.translate(message_text).text)
+    else:
+        send_message(user_id, name + translator.translate(message_text, dest=lang).text)
 
 def verify_fb_token(token_sent):
     #take token sent by facebook and verify it matches the verify token you sent
@@ -180,15 +75,6 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-#chooses a random message to send to the user
-def reply_message(message):
-    if int(MAINTENANCE_MODE):
-        return "help"
-    else:
-        # return selected item to the user
-        return message
-
-#uses PyMessenger to send response to user
 def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
---
+++ database.py
@@ -0,0 +1,157 @@
+import pyrebase
+import app
+import languages
+import os
+
+API_KEY = os.environ['API_KEY']
+AUTHDOMAIN = os.environ['AUTHDOMAIN']
+DATABASEURL = os.environ['DATABASEURL']
+PROJECTID = os.environ['PROJECTID']
+STORAGEBUCKET = os.environ['STORAGEBUCKET']
+MESSAGINGSENDERID = os.environ['MESSAGINGSENDERID']
+
+config = {
+    "apiKey": API_KEY,
+    "authDomain": AUTHDOMAIN,
+    "databaseURL": DATABASEURL,
+    "projectId": PROJECTID,
+    "storageBucket": STORAGEBUCKET,
+    "messagingSenderId": MESSAGINGSENDERID
+};
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+def parse_command(sender_id, message_text):
+    '''This parses commands to do things'''
+    if message_text == "/leave_room":
+        leave_room(sender_id)
+    elif message_text == "/join_room":
+        app.send_message(sender_id, "Please enter your room number. Usage: /join_room <room_number>")
+    elif message_text == "/set_name":
+        app.send_message(sender_id, "Please enter your new name. Usage: /set_name <new_name>")
+    elif message_text == "/get_commands":
+        get_commands(sender_id)
+    elif message_text == "/show_langs":
+        send_language_list(sender_id)
+    elif message_text.split(" ")[0] == "/join_room":
+        room_id = message_text.split(" ", 1)[1]
+        join_room(sender_id, room_id)
+    elif message_text.split(" ")[0] == "/set_name":
+        new_name = message_text.split(" ", 1)[1]
+        change_name(sender_id, new_name)
+    elif message_text.split(" ")[0] == "/set_lang":
+        new_lang = message_text.split(" ", 1)[1]
+        set_language(sender_id, new_lang)
+
+def send_language_list(user_id):
+    '''Sends a user a list of languages and their codes'''
+    language_list = "```\nLanguage: Code\n"
+    for lang, code in languages.LANGCODES.items():
+        language_list += (lang + ": " + code + "\n")
+    language_list += "```"
+    app.send_message(user_id, language_list)
+
+def join_room(user_id, room_id):
+    '''This allows our user to join a room'''
+
+    # Get our room
+    room = db.child("rooms").child(room_id).get().val()
+
+    # If our room does not exist, create a new one
+    if room == None:
+        db.child("rooms").child(room_id).update({"users": [user_id]})
+        return False
+    else:
+        # Otherwise, set our room_id and join the room
+        room.get("users").append(user_id)
+        db.child("rooms").child(room_id).update(room)
+        db.child("users").child(user_id).update({"room_id": room})
+        app.send_message(user_id, "Successfully joined room " + room_id)
+        print("Successfully added " + user_id + " to " + room_id)
+        return True
+
+def change_name(user_id, new_name):
+    ''' Allows a user to change their name '''
+    db.child("users").child(user_id).update({"name": new_name})
+
+def leave_room(user_id):
+    ''' Lets a user leave a room '''
+    # Get our room id
+    room_id = get_user_room(user_id)
+    if room_id == None:
+        # If it doesn't exist, then ask them to join a room first
+        app.send_message(user_id, "Please join a room first!")
+        return False
+    else:
+        # Remove from room and reset room id
+        room = db.child("rooms").child(room_id).get().val()
+        room.remove(user_id)
+        db.child("users").child(user_id).update({"room_id": None})
+        app.send_message(user_id, "Successfully left room " + room_id)
+        return True
+
+def set_language(user_id, language):
+    ''' Allows a user to change their name '''
+    if language not in languages.LANGUAGES.keys():
+        app.send_message(user_id, "Please enter a valid language. Language codes can be found by typing /show_langs")
+    else:
+        db.child("users").child(user_id).update({"lang": language})
+        app.send_message(user_id, "Successfully set language to " + languages.LANGUAGES.get(language))
+
+def get_user_room(user_id):
+    ''' Returns the Room ID of the user'''
+    return db.child("users").child(user_id).get().val().get("room_id")
+
+def get_room(room_id):
+    ''' Returns a room from an ID '''
+    return db.child("rooms").child(room_id).get().val()
+
+def get_name(user_id):
+    ''' Returns the name of the user '''
+    return db.child("users").child(user_id).get().val().get("name")
+
+def get_lang(user_id):
+    ''' Returns the name of the user '''
+    return db.child("users").child(user_id).get().val().get("lang")
+
+def get_db_size():
+    return str(len(db.child("users").get().val().keys()))
+
+def check_new_user(user_id):
+    if user_id in db.child("users").get().val().keys():
+        # We don't have a new user
+        return False
+    else:
+        # We do have a new user!
+        db.child("users").child(user_id).set({"name": "User " + get_db_size()})
+        db.child("users").child(user_id).update({"lang": "en"})
+        welcome_message = """Welcome to Tell! Your language has been set to English. 
+        Type '/get_commands' to get a list of commands!"""
+        app.send_message(user_id, welcome_message)
+        return True
+
+def get_commands(user_id):
+    commands = """
+    ```
+    /join_room <room number>: Join a room
+    /leave_Room: Leave a room
+    /set_name <name>: Set your name
+    /set_lang <language>: Set your language. For a full list of languages type '/show_langs'
+    ```"""
+    app.send_message(user_id, commands)
+
+def check_room(user_id, room_id):
+    '''Checks if a user is in a room'''
+
+    room = db.child("rooms").child(room_id).get().val()
+    if room == None:
+        # our room doesn't exist
+        return False
+    elif user_id in room:
+        # Our user is in the room!
+        return True
+    else:
+        # Our user isn't in the room :(
+        return False
